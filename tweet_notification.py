963e9565cdc3cb5ec7703ee010df79a1a455a1eb
Amir <amir.rahmani@stonybrook.edu>
2018-03-28 18:59:12 -0400

    remove credentials

--- tweet_notification.py
+++ tweet_notification.py
@@ -11,22 +11,22 @@ def notify(title, subtitle, link):
     os.system('terminal-notifier {}'.format(' '.join([t, s, l ,so])))
 
 # account info for text message
-accountSID = 'AC00aec6d1f37eab426f064da2b915b146'
-authToken = '3ec9cec420a117d374949b65fa06544f'
+accountSID = 'YOUR accountSID'
+authToken = 'YOUR authToken'
 twilioCli = TwilioRestClient(accountSID, authToken)
-myTwilioNumber = '+16012024175'
-myCellPhone = '+16628019241'
+myTwilioNumber = 'YOUR TwilioNumber'
+myCellPhone = 'YOUR Cell Phone number'
 
 # authenticate twitter
-API_KEY='0QXnDxqXw9S2dsVpSTumYAgwA' #25 alphanumeric characters
-API_SECRET='AJmWZk0qS70LzOediTMsz3Ky2THDJTkjZfKj9Asf5znySqSQ53' #50 alphanumeric characters
-ACCESS_TOKEN = '1510941108-MdxwpWheudi9ptPNjkOcdb0I9nuNgFvsTreGeSn'
-ACCESS_TOKEN_SECRET = 'lexqzMsBAiWpOUX0MpAI2aQbYNrjs4DajhMrrwnAPOpj1'
+API_KEY='YOUR API KEY' #25 alphanumeric characters
+API_SECRET='YOUR API_SECRET' #50 alphanumeric characters
+ACCESS_TOKEN = 'YOUR ACCESS_TOKEN'
+ACCESS_TOKEN_SECRET = 'YOUT ACCESS_TOKEN_SECRET'
 api = TwitterAPI(API_KEY, API_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
 
-
-usr_id = 25073877 # realdonaldtrump
-usr_id = 818910970567344128 # vp
+user_id = 2899773086 # every3minutes
+#usr_id = 25073877 # realdonaldtrump
+#usr_id = 818910970567344128 # vp
 #usr_id = 822215679726100480 # potus
 r = api.request('statuses/filter', {'follow':str(usr_id)}).get_iterator()
 for item in r:
