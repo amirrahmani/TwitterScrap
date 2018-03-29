6ebb34a70c7fc2fe34f2ccf2e7743a168f66cf46
Amir <amir.rahmani@stonybrook.edu>
2018-03-29 04:59:09 -0400

    update collections

--- datacollection.py
+++ datacollection.py
@@ -1,48 +1,51 @@
-# to collect last 3200 tweets of an account and save tweet dates and texts to a file
+# To collect tweets and save them as txt file
 from twython import Twython
 import sys
 import time
-import simplejson
+import json
 from math import ceil
 
-sys.argv = ['','realdonaldtrump'] # by removing this you can pass twitter 
+sys.argv = ['','realdonaldtrump'] # by removing this you can pass twitter
                                   # username at command line
 
-parentpathi ='your path'
-handle = sys.argv[1] # takes target twitter screenname as command-line argument
-today = time.strftime("%Y%m%d")
-csvfilepath = parentpath+'/'+handle+'_'+today+'.csv'
-txtfilepath = parentpath+'/'+handle+'_'+today+'.txt'
+handle = sys.argv[1] #takes target twitter screenname as command-line argument
+today = time.strftime("%Y%m%d") # today date
+txtfilepath = handle + '_' + today + '.txt' # txt filename
+jsonfilepath = handle + '_' + today + '.p' # json filename
  
-#authenticate
-APP_KEY = #25 alphanumeric characters
-APP_SECRET = #50 alphanumeric characters
+################# Authenticate ####################################################
+APP_KEY= #25 alphanumeric characters
+APP_SECRET= #50 alphanumeric characters
 twitter=Twython(APP_KEY,APP_SECRET,oauth_version=2) #simple authentication object
 ACCESS_TOKEN=twitter.obtain_access_token()
 twitter=Twython(APP_KEY,access_token=ACCESS_TOKEN)
  
-#adapted from http://www.craigaddyman.com/mining-all-tweets-with-python/
-#user_timeline=twitter.get_user_timeline(screen_name=handle,count=200) #if doing 200 or less, just do this one line
-user_timeline=twitter.get_user_timeline(screen_name=handle,count=1) #get most recent tweet
-lis=user_timeline[0]['id']-1 #tweet id # for most recent tweet
-#only query as deep as necessary
-tweetsum= user_timeline[0]['user']['statuses_count']
-if tweetsum <200 :
+# Adapted from http://www.craigaddyman.com/mining-all-tweets-with-python/
+user_timeline = twitter.get_user_timeline(screen_name=handle,count=1) # get most recent tweet
+lis = user_timeline[0]['id']-1 #tweet id # for most recent tweet
+
+# Query as deep as necessary
+tweetsum= user_timeline[0]['user']['statuses_count'] # Total number of tweets
+if tweetsum <200 :  #API only allows depth of 3200 so no point trying deeper than 200*16
       cycles = 1
 else:
       cycles=int(ceil(tweetsum / 200))
 
 if cycles>16:
-    cycles=16 #API only allows depth of 3200 so no point trying deeper than 200*16
-#time.sleep(60)
-for i in range(0, cycles): ## iterate through all tweets up to max of 3200
-    incremental = twitter.get_user_timeline(screen_name=handle,
-    count=200, include_retweets=True, max_id=lis)
-    user_timeline.extend(incremental)
-    lis=user_timeline[-1]['id']-1
-    print 'sleeping'
-    time.sleep(75) # 75 second rest between api calls. The API allows 15 calls per 15 minutes
- 
+      cycles=16
+
+for i in range(cycles): # iterate through each cycle and read 200 tweets each time
+      incremental = twitter.get_user_timeline(screen_name=handle,
+      count=200, include_retweets=True, max_id=lis)
+      user_timeline.extend(incremental)
+      lis = user_timeline[-1]['id']-1 # update the last tweet id
+      if i<cycles-1:
+            print 'sleeping'
+            time.sleep(75) # 75 second rest between api calls. The API allows 15 calls per 15 minutes
+
+user_timeline = reversed(user_timeline)
+
+tweet = {'date':[],'text':[]}
 
 with open(txtfilepath,'wt') as fid:
       for d in user_timeline:
@@ -53,3 +56,4 @@ with open(txtfilepath,'wt') as fid:
 
 with open(jsonfilepath,'w') as fid:
       json.dump(tweet,fid)
+
--- datacollection_update.py
+++ datacollection_update.py
@@ -1,6 +1,4 @@
-# Collecting tweets from an account and compare with the previously collected
-# tweets and update the file with only new tweets.
-
+#datacollection.py
 from twython import Twython
 import sys
 import time
@@ -8,32 +6,33 @@ import json
 import datetime 
 from math import ceil
 
+
+sys.argv = ['','realdonaldtrump'] # by removing this you can pass twitter
+                                  # username at command line
+#authenticate twitter
+APP_KEY= #25 alphanumeric characters
+APP_SECRET= #50 alphanumeric characters
+twitter=Twython(APP_KEY,APP_SECRET,oauth_version=2) #simple authentication object
+ACCESS_TOKEN=twitter.obtain_access_token()
+twitter=Twython(APP_KEY,access_token=ACCESS_TOKEN)
+
 breakIdx = 0
-parentpath='YOUR PATH'
-handle=sys.argv[1] #takes target twitter screenname as command-line argument
-txtfilepath=parentpath+'/'+handle+'_tweet_dates.txt'
+handle = sys.argv[1] #takes target twitter screenname as command-line argument
+jsonfilepath = handle + '_tweet_json.p' # json filename
+txtfilepath = handle + '_tweet_dates.txt'
 new_date = []
 
 with open(jsonfilepath,'r') as fid:
       tweet = json.load(fid)
- 
-#authenticate
-APP_KEY = #25 alphanumeric characters
-APP_SECRET = #50 alphanumeric characters
-twitter=Twython(APP_KEY,APP_SECRET,oauth_version=2) #simple authentication object
-ACCESS_TOKEN=twitter.obtain_access_token()
-twitter=Twython(APP_KEY,access_token=ACCESS_TOKEN)
- 
-#adapted from http://www.craigaddyman.com/mining-all-tweets-with-python/
-#user_timeline=twitter.get_user_timeline(screen_name=handle,count=200) #if doing 200 or less, just do this one line
-user_timeline=twitter.get_user_timeline(screen_name=handle,count=1) #get most recent tweet
-lis=user_timeline[0]['id']-1 #tweet id # for most recent tweet
-#time.sleep(60)
+
+
+user_timeline = twitter.get_user_timeline(screen_name=handle,count=1) #get most recent tweet
+lis = user_timeline[0]['id']-1 #tweet id # for most recent tweet
 incremental = twitter.get_user_timeline(screen_name=handle,
       count = 200, include_retweets = True, max_id=lis)
 user_timeline.extend(incremental)
 lis = user_timeline[-1]['id']-1
-date = user_timeline[-1]['created_at'].encode('ascii','ignore').decode("utf-8") 
+date = user_timeline[-1]['created_at'].encode('ascii','ignore')
 date = datetime.datetime.strptime(date.strip(),"%a %b %d %H:%M:%S +0000 %Y")
 
 # read the dates from previous file
@@ -54,6 +53,6 @@ with open(txtfilepath,'w') as fid:
       for d in date_old:
             fid.write(d)
             fid.write("\n")
-            
+
 with open(jsonfilepath,'w') as fid:
-      json.dump(tweet,fid)            
+      json.dump(tweet,fid)
